1f48a884c4bd3d4421822c43b4920cefecbe0ff9
João Victor Corsi Ferreira <jvccorsiferrera@hotmail.com>
2020-10-23 22:38:35 -0300

    Melhoramento na classe receber_dados

--- src/Project/receber_dados.java
+++ src/Project/receber_dados.java
@@ -7,20 +7,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class receber_dados {
+	
 	private List<Palavras> listPalavras = new ArrayList<Palavras>(); //Instanciando o ArrayList com a class Palavras
 	
 	
+	public String reader_path(String caminho) {
+		String path = caminho;
+		return path;
+	}
 	public void pegar_dados_arq() {	
-		String path = "C:\\Users\\jvcco\\Desktop\\Ex4_Parabens.txt"; //Caminho do arquivo.
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) { //Dessa maneira, já instancia os recursos no bloco try.
+		try (BufferedReader br = new BufferedReader(new FileReader(reader_path("C:\\Users\\jvcco\\Desktop\\Ex4_Parabens.txt")))) { //Dessa maneira, já instancia os recursos no bloco try.
 			//Primeira linha															//Quando terminar o bloco try ou cair no catch já haverá a desalocação dos recursos.
 			String line = br.readLine(); //Ler de linha a linha. ReadLine() le a string até a quebra de linha								
-			while (line != null) { //Ler até o final do arquivo.
+			while (line != null) { //Ler até o final do arquivo.		
 				String[] palavrasDaLinha  = line.split(" "); //Recorta a string em partes dividindo por espaço em branco e armazena em um vetor.
 				//Vai pegar cada palavra da linha, dividindo por espaço em branco, e armazenar em um vetor.
 				for(String palavra : palavrasDaLinha) { //Para cada palavra na string PalavrasdaLinha faça:
-		            if(!"".equals(palavra.trim())) { //Se não for espaço em branco, salve na listPalavras
-		            	
+		            if(!"".equals(palavra.trim())) { //Se não for espaço em branco, salve na listPalavras	
 		            	Palavras palavras_class= new Palavras(palavra);
 		            	this.listPalavras.add(palavras_class);  //Adicionar o Objeto no ArrayList
 		            }
@@ -47,7 +50,6 @@ public class receber_dados {
 	public void exibir_elementos() {
 		for (int i = 0; i <getListPalavras().size(); i++) {
 			System.out.println(getListPalavras().get(i));	
-
 		}
 			
 	}
