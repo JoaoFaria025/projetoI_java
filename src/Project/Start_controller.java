00f321a03329ab5f0322046143b7b04d96884157
João Victor Corsi Ferreira <jvccorsiferrera@hotmail.com>
2020-10-25 13:58:05 -0300

    FEATURES: Integrei a funcao do kaka no receber_dados

--- src/Project/Start_controller.java
+++ src/Project/Start_controller.java
@@ -1,13 +1,27 @@
 package Project;
 
+import javax.swing.JOptionPane;
+
+
+
 public class Start_controller {
+	private String pathString;
+	
+	public String getPathString() {
+		return pathString;
+	}
+
+
+	public void setPathString(String pathString) {
+		this.pathString = pathString;
+	}
+
+
 	public  void start () {
+	setPathString(JOptionPane.showInputDialog("Digite o caminho do arquivo:"));
 	receber_dados dados = new receber_dados();
-	dados.pegar_dados_arq();
+	dados.pegar_dados_arq(getPathString());
 	dados.exibir_elementos();
-	/*dados.formatar_words();
-	String aux = dados.removeAcents("√¡¡");//FUNCIONANDO RETIRAR ACENTOS
-	System.out.println(aux);*/
 	}
 		
 }
--- src/Project/receber_dados.java
+++ src/Project/receber_dados.java
@@ -5,14 +5,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.text.Normalizer;
 import java.util.ArrayList;
-import java.util.List;
 
 public class receber_dados {
-	private List<Palavras> listPalavras = new ArrayList<Palavras>(); //Instanciando o ArrayList com a class Palavras
-	private String aux = "ááÀ.";//CORSI -----> retirar serve so pra ver q ta rodando
 	
-	public void pegar_dados_arq() {	
-		String path = "C:\\Users\\htols\\Desktop\\Ex4_Parabens.txt"; //Caminho do arquivo.
+	private ArrayList<String> listPalavras = new ArrayList<String>(); //Instanciando o ArrayList com a class Palavras
+	
+	public void pegar_dados_arq(String path) {	
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) { //Dessa maneira, já instancia os recursos no bloco try.
 			//Primeira linha															//Quando terminar o bloco try ou cair no catch já haverá a desalocação dos recursos.
 			String line = br.readLine(); //Ler de linha a linha. ReadLine() le a string até a quebra de linha								
@@ -21,9 +19,7 @@ public class receber_dados {
 				//Vai pegar cada palavra da linha, dividindo por espaço em branco, e armazenar em um vetor.
 				for(String palavra : palavrasDaLinha) { //Para cada palavra na string PalavrasdaLinha faça:
 		            if(!"".equals(palavra.trim())) { //Se não for espaço em branco, salve na listPalavras
-		            	
-		            	Palavras palavras_class= new Palavras(palavra);
-		            	this.listPalavras.add(palavras_class);  //Adicionar o Objeto no ArrayList
+		            	this.listPalavras.add(palavra);  //Adicionar o Objeto no ArrayList
 		            }
 				}
 				line = br.readLine(); //Quando line receber o null ele sai do arquivo. Significa que acabou o arquivo.
@@ -34,31 +30,28 @@ public class receber_dados {
 			System.out.println("Error: " + e.getMessage());
 		}
 	}
-	public static String removeAcents(String str) { //CORSI -----> POE A LISTA P RECEBER
-	    return Normalizer.normalize(str, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+	public static String removeAcents(String palavras) { //CORSI -----> POE A LISTA P RECEBER
+	    return Normalizer.normalize(palavras, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
 	}
 	
-	public void formatar_words() {
-		System.out.println("-----------\nRECEBER_DADOS\n-----------");
-		String recebe = aux.toLowerCase();//mínusculas CORSI -----> POE A LISTA P RECEBER 
-		System.out.println(aux);//TESTE P/ VERIFICAR SE DEIXA MINUSCULO	---> RETIRAR DPS
+	public String formatar_words(String palavras) {
+		return palavras.toLowerCase();
 	}
 	
-	public List<Palavras> getListPalavras() {
+	public ArrayList<String> getListPalavras() {
 		return listPalavras;
 	}
 
-	public void setListPalavras(List<Palavras> listPalavras) {
+	public void setListPalavras(ArrayList<String> listPalavras) {
 		this.listPalavras = listPalavras;
 	}
 	
 	public void exibir_elementos() {
 		for (int i = 0; i <getListPalavras().size(); i++) {
-			System.out.println(getListPalavras().get(i));	
-
-	}
-		
-			
+			getListPalavras().set(i, removeAcents(getListPalavras().get(i)));
+			getListPalavras().set(i, formatar_words(getListPalavras().get(i)));
+			System.out.println(getListPalavras().get(i));
+	}		
 	}
 }
 	
